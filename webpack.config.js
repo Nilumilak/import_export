22936ae21d74c9c390a5d5dcc6c9d0b116ab76d1
Renat Kalimulin <pianoparrot93@gmail.com>
2023-04-14 13:24:12 +0300

    Fixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'index.js'
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
+            filename: 'main.html'
         }),
     ]
 }
